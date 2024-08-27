1a5a41577bc03a3e48d254643322bcdd100e6c98
Ann Castro <anncasstro@gmail.com>
2024-08-27 16:14:55 -0500

    Trabajo Event Binding y se agrega carpeta public con iconos

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
   templateUrl: './card-product.component.html',
-  styleUrl: './card-product.component.css'
+  styleUrls: ['./card-product.component.css'] // Nota: Cambié `styleUrl` a `styleUrls`
 })
-export class CardProductComponent {
+export class CardProductComponent implements OnInit {
 
+  cantidad: number = 0;
+
+  product = {
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addProduct() {
+    this.cantidad++;
+  }
+
+  removeProduct() {
+    if (this.cantidad > 0) { 
+      this.cantidad--;
+    }
+  }
 }
